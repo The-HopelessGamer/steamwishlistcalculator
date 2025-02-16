ebeeeb8f05f7fe781af80255c645c25c5d44b831
Sean <thehopelessgamer@outlook.com>
2025-02-16 22:03:22 +1000

    Loading

--- frontend/src/content_views/wishlist/wishlist.tsx
+++ frontend/src/content_views/wishlist/wishlist.tsx
@@ -1,18 +1,8 @@
 import "./wishlist.css";
 import { useParams } from "react-router";
-import { Loader } from "../../design_system/loader/loader";
-import { ContentBox } from "../../design_system/content_box/content_box";
 
 export function Wishlist() {
 	const { wishlistId } = useParams();
 
-	return (
-		<span className="wishlistLoaderContainer">
-			<ContentBox color="white">
-				<div className="wishlistCalculatingTitle">
-					Calculating Your Wishlist <Loader color="black" />
-				</div>
-			</ContentBox>
-		</span>
-	);
+	return <span>Wishlist</span>;
 }
