4dff934ccce475156a4a6e794d3319988c0ec7bf
Sean <thehopelessgamer@outlook.com>
2025-05-05 14:33:15 +1000

    Bug fixes

--- frontend/src/header/currency_dropdown/currency_dropdown.tsx
+++ frontend/src/header/currency_dropdown/currency_dropdown.tsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { classNames } from "../../utils";
 import "./currency_dropdown.css";
 import { DropdownProps, Dropdown } from "../../design_system/dropdown/dropdown";
 
 export function CurrencyDropdown(props: DropdownProps) {
 	const [isOpen, setIsOpen] = useState(false);
+	const dropdownRef = useRef<HTMLDivElement>(null);
 	const selectedOption = props.options.find((opt) => opt.value === props.value);
 
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				dropdownRef.current &&
+				!dropdownRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => document.removeEventListener("mousedown", handleClickOutside);
+	}, []);
+
 	return (
-		<div className="currencyDropdownContainer">
+		<div className="currencyDropdownContainer" ref={dropdownRef}>
 			<div
 				className="currencyDropdownTrigger"
 				onClick={() => setIsOpen(!isOpen)}
